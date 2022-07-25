ecbb3b3572f8f9c4718adf0239d79071dc6ca9c0
OmarMWarraich <earthling986@gmail.com>
2022-07-26 04:37:36 +0500

    feat: hamburger menu

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-let button = document.getElementById("union");
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
 
-button.addEventListener('click', function() {
-    console.log('clicked');
-} );
+console.log("navToggle", navToggle);
 
+navToggle.addEventListener('click', function(){
+    links.classList.toggle("show_nav");
+})
+
+const navLink = document.querySelector('.nav_links');
+
+console.log('navLink', navLink);
+
+navLink.addEventListener('click', function(){
+    links.classList.remove("show_nav");
+})
