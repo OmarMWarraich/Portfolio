ce7530da0636fea199b49d07702a255ffbc24f07
OmarMWarraich <earthling986@gmail.com>
2022-07-27 02:21:29 +0500

    feat: add hamburger activity

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const navToggle = document.querySelector(".navbar_toggle");
 const links = document.querySelector(".main_nav");
 
-console.log("navToggle", navToggle);
-
 navToggle.addEventListener('click', function(){
     links.classList.toggle("show_nav");
 })
 
-const navLink = document.querySelector('.nav_links');
+const navLinks = document.querySelectorAll('.main_nav > li');
 
-console.log('navLink', navLink);
-
-navLink.addEventListener('click', function(){
+navLinks.addEventListener('click', function(){
     links.classList.remove("show_nav");
 })
